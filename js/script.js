e370657816f1f18594642fa053199af3b9636bcd
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-14 20:19:16 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,20 +1,47 @@
-var numeroSecreto = parseInt(Math.random() * 1001);
-var tentativas = 10;
+var numeroEscolhido;
+var chute;
+var tentativas;
+var apenasNumeros;
 
-function chutou() {
-  var chute = parseInt(document.getElementById("chute").value);
-  if (chute == numeroSecreto) {
-    alert("Você acertou o chute o número secreto é " + chute);
-    window.location.reload();
-  } else if (chute > numeroSecreto) {
-    alert("O número " + chute + " é maior que o número secreto");
-    tentativas = tentativas - 1;
-  } else if (chute < numeroSecreto) {
-    alert("O número " + chute + " é menor que o número secreto");
-    tentativas = tentativas - 1;
-  }
-  if (tentativas <= 0) {
-    alert("Você perdeu o número secreto era " + numeroSecreto);
-    window.location.reload();
-  }
+numeroEscolhido = Math.floor(Math.random() * 1001);
+tentativas = 1;
+
+
+function verificarInput(apenasNumeros) {
+  return /^\d+$/.test(apenasNumeros);
 }
+
+
+while (chute != numeroEscolhido) {
+  chute = prompt(
+    "Digite um número de 1 à 1000.\n\nSua tentativa é a de número: " +
+      tentativas
+  );
+
+  if (!verificarInput(chute) || chute < 0 || chute > 1000 || chute == 0) {
+    alert("Você digitou um número inválido!");
+    continue;
+  }
+
+  if (chute == numeroEscolhido) {
+    alert(
+      "Parabéns, você descobriu! O número era " +
+        numeroEscolhido +
+        " e você precisou de " +
+        tentativas +
+        " tentativas!"
+    );
+    break;
+  } else if (tentativas >= 13) {
+    alert("Você perdeu! O número era " + numeroEscolhido);
+    break;
+  } else if (chute > numeroEscolhido) {
+    alert("O número escolhido é menor que " + chute + "!");
+  } else if (chute < numeroEscolhido) {
+    alert("O número escolhido é maior que " + chute + "!");
+  }
+
+  tentativas++;
+}
+
+window.location.reload();
